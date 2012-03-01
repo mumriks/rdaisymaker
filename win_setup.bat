82bb21f87aca56101db87f38747963875ce37b10
mumriks <qublilabo@gmail.com>
2012-03-01 17:40:37 +0900

    Ruby インストールパスの 32/64ビットへの暫定的な対応

--- win_setup.bat
+++ win_setup.bat
@@ -1,3 +1,3 @@
 @rem rdmconsole.bat 生成用バッチファイル
-@set path=%path%;c:\Program Files (x86)\Ruby-1.9.3\bin;
+@set path=%path%;c:\Program Files (x86)\Ruby-1.9.3\bin;c:\Program Files\Ruby-1.9.3\bin;
 ruby ws.rb
--- ws.rb
+++ ws.rb
@@ -3,11 +3,21 @@
 #
 cdir = Dir.pwd
 bindir = File.expand_path("./bin", cdir)
+path32 = "c:/Program Files/Ruby-1.9.3/bin"
+path64 = "c:/Program Files (x86)/Ruby-1.9.3/bin"
+if File.exists?(path32)
+   rubypath = path32
+elsif File.exists?(path64)
+   rubypath = path64
+else
+   puts "先に Ruby をインストールしてください。"
+   exit 0
+end
 
 File.open("rdmconsole.bat", "w"){|f|
    f.puts <<EOT
 @rem R DAISY Maker 実行コンソール起動バッチファイル
-@set path=%path%;c:/Program Files (x86)/Ruby-1.9.3/bin;#{bindir};
+@set path=%path%;#{rubypath};#{bindir};
 start cmd
 EOT
 }
